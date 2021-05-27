d51106d812d6a90d04df2835a0e0df1e5cfd65a0
eternitybeingness <theta.box@gmail.com>
2021-05-27 16:22:39 +0200

    3.3. Загрузка файл Multiple.java на сайт github

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Roman Karchevskiy");
+        System.out.println("05.12.1978");
+    }
+}
