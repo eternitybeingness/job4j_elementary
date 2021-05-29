81a461a3fccf3faccfa359c77833620ccc370a5e
eternitybeingness <theta.box@gmail.com>
2021-05-29 14:04:23 +0200

    4.1. Аргументы [#295521]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -3,7 +3,7 @@ package ru.job4j.converter;
 public class Converter {
 
     public static int rubleToEuro(int value) {
-        return value / 70;
+        return value / 70; /* формула перевода рублей в евры. */
     }
 
     public static int rubleToDollar(int value) {
