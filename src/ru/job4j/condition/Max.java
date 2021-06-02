711b9999274b64feb00fee66ded2fd5968284d87
eternitybeingness <theta.box@gmail.com>
2021-06-03 01:00:47 +0200

    4.2. Тернарное сравнение [#189]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,8 +2,6 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        boolean condition = left > right;
-        int result = condition ? left : right;
-        return result;
+        return left > right ? left : right;
     }
 }
