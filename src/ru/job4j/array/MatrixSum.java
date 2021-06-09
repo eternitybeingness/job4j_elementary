870039a194fe5011f41e25564a27fc1b43bf40f6
eternitybeingness <theta.box@gmail.com>
2021-06-09 03:19:04 +0200

    6.6.2. Двухмерный массив. Циклы. [#306725]

--- src/ru/job4j/array/MatrixSum.java
+++ src/ru/job4j/array/MatrixSum.java
@@ -4,8 +4,8 @@ public class MatrixSum {
     public static int sum(int[][] array) {
         int rsl = 0;
         for (int[] ints : array) {
-            for (int j = 0; j < array[0].length; j++) {
-                rsl = rsl + ints[j];
+            for (int anInt : ints) {
+                rsl = rsl + anInt;
             }
         }
         return rsl;
