9dfa91d6c8c7ab34dfc44059540638f2608324b9
eternitybeingness <theta.box@gmail.com>
2021-06-11 04:25:36 +0200

    6.8. Дефрагментация массива [#225]

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length - 1; index++) {
+            if (array[index] == null) {
+                for (int j = index + 1; j < array.length; j++) {
+                    if (array[j] != null) {
+                        array[index] = array[j];
+                        array[j] = null;
+                        break;
+                    }
+                }
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (String s : compressed) {
+            System.out.print(s + " ");
+        }
+    }
+}
