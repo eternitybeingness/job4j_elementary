eb57c49ffe9588713b7c172dd9b09318436d8a86
Roma <theta.box@gmail.com>
2021-08-01 02:45:24 +0200

    3. Перегрузить метод max для трех чисел [#122644]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -4,4 +4,12 @@ public class Max {
     public static int max(int left, int right) {
         return left > right ? left : right;
     }
+
+    public static int max(int left, int right, int third) {
+        return max(left, right) > third ? max(left, right) : third;
+    }
+
+    public static int max(int left, int right, int third, int forth) {
+        return max(left, right, third) > forth? max(left, right, third) : forth;
+    }
 }
--- test/ru/job4j/condition/MaxTestNew.java
+++ test/ru/job4j/condition/MaxTestNew.java
@@ -1,15 +1,18 @@
-/*package ru.job4j.condition;
+package ru.job4j.condition;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-public class ThreeMaxTest {
+import static org.junit.Assert.*;
+
+public class MaxTestNew {
     @Test
     public void firstMax() {
         int first = 10;
         int second = 5;
         int third = 1;
-        int result = ThreeMax.max(first, second, third);
+        int forth = 4;
+        int result = Max.max(first, second, third, forth);
         int expected = 10;
         Assert.assertEquals(expected, result);
     }
@@ -19,7 +22,8 @@ public class ThreeMaxTest {
         int first = 10;
         int second = 50;
         int third = 1;
-        int result = ThreeMax.max(first, second, third);
+        int forth = 25;
+        int result = Max.max(first, second, third, forth);
         int expected = 50;
         Assert.assertEquals(expected, result);
     }
@@ -29,8 +33,9 @@ public class ThreeMaxTest {
         int first = 1;
         int second = 5;
         int third = 100;
-        int result = ThreeMax.max(first, second, third);
-        int expected = 100;
+        int forth = 500;
+        int result = Max.max(first, second, third, forth);
+        int expected = 500;
         Assert.assertEquals(expected, result);
     }
 
@@ -39,7 +44,8 @@ public class ThreeMaxTest {
         int first = 1;
         int second = 1;
         int third = 1;
-        int result = ThreeMax.max(first, second, third);
+        int forth = 1;
+        int result = Max.max(first, second, third, forth);
         int expected = 1;
         Assert.assertEquals(expected, result);
     }
@@ -49,17 +55,19 @@ public class ThreeMaxTest {
         int first = 10;
         int second = 10;
         int third = 1;
-        int result = ThreeMax.max(first, second, third);
+        int forth = 1;
+        int result = Max.max(first, second, third, forth);
         int expected = 10;
         Assert.assertEquals(expected, result);
     }
 
     @Test
-    public void firstEqThird() {
+    public void firstEqThirdForth() {
         int first = 100;
         int second = 1;
         int third = 100;
-        int result = ThreeMax.max(first, second, third);
+        int forth = 100;
+        int result = Max.max(first, second, third, forth);
         int expected = 100;
         Assert.assertEquals(expected, result);
     }
@@ -69,8 +77,9 @@ public class ThreeMaxTest {
         int first = 1;
         int second = 100;
         int third = 100;
-        int result = ThreeMax.max(first, second, third);
+        int forth = 1;
+        int result = Max.max(first, second, third, forth);
         int expected = 100;
         Assert.assertEquals(expected, result);
     }
-}*/
+}
